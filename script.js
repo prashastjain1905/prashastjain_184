a03abb9d75ba5611b0495ddb563cf994782739e2
prashastjain1905 <jainprashast1905@gmail.com>
2025-10-14 10:03:38 +0530

    paalak paneer

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function increment(id) {
   const counter = document.getElementById(id);
   counter.textContent = parseInt(counter.textContent) + 1;
 }
+
+function decrement(id) {
+  const counter = document.getElementById(id);
+  let value = parseInt(counter.textContent);
+  if (value > 0) {
+    counter.textContent = value - 1;
+  }
+}
+
+function reset(id) {
+  document.getElementById(id).textContent = 0;
+}
